6b5daa5e63089d5508b0a3029ed3666fcc4f5952
zRanqa <jonnoach@gmail.com>
2025-08-15 06:19:23 -0700

    added the bottom bar with some text

--- MultiScreenApp/ContentView.swift
+++ MultiScreenApp/ContentView.swift
@@ -16,6 +16,13 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        Spacer()
+        HStack {
+            Text("1")
+            Text("2")
+            Text("3")
+            Text("4")
+        }
     }
 }
 
